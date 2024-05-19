e11b60046e99f07ef40526e70196cc33aeaaab5f
Hope | X6739-Team | #TeamPear <Hopexox23@gmail.com>
2024-05-19 10:45:53 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# android_twrp_infinix_x6739-twrp-12.1
+ 
---
+++ twrp_X6739.mk
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from Infinix-X6739 device
+$(call inherit-product, device/infinix/X6739/device.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Virtual A/B OTA
+# https://source.android.com/docs/core/ota/virtual_ab/implement#build-flags
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Enable updating of APEXes
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+PRODUCT_DEVICE := X6739
+PRODUCT_NAME := twrp_X6739
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix GT 10 Pro
+PRODUCT_MANUFACTURER := infinix
+
+PRODUCT_GMS_CLIENTID_BASE := android-infinix
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_x6739_h931-user 12 SP1A.210812.016 537496 release-keys" \
+    TARGET_DEVICE=Infinix-X6739 \
+    PRODUCT_NAME=X6739-GL
+
+BUILD_FINGERPRINT := "Infinix/X6739-GL/Infinix-X6739:12/SP1A.210812.016/240204V1917:user/release-keys"
